8469f0f1a6b01008ffd2ba59b73560ad252f3fca
Andrés Felipe García Granados - Ceiba Software <andres.granados@ceiba.com.co>
2019-04-23 20:44:00 -0500

    taller1

---
+++ PooEShopping/src/java/org/pl/eshop/dto/CategoriaDAO.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pl.eshop.dto;
+
+/**
+ *
+ * @author andres.granados
+ */
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import org.pl.eshop.bd.Conexion;
+
+public interface CategoriaDAO {
+
+    public void agregar(Categoria c) throws Exception;
+
+    public void modificar(Categoria c) throws Exception;
+
+    public void eliminar(Categoria c) throws Exception;
+
+    public List<Categoria> obtenerTodas() throws Exception;
+
+    public Categoria obtenerPorId(Integer id) throws Exception;
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # admin_categorias
+
+Taller 1 de Desarrollo de Software basado en Componentes
